6088cc7dd6353e33eeac6a4395456ffbb71212f9
Leonardo Matos <leomp120894@gmail.com>
2020-01-08 12:30:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import Vue from 'vue'
 import '@ecomplus/storefront-twbs'
 import MyComponent from './components/MyComponent.vue'
 
-export default options => {
-  const elId = 'some-el-id'
-  new Vue({
-    components: {
-      MyComponent
-    },
-    template: `
-    <my-component id="${elId}"/>`
-  }).$mount(`#${elId}`)
+export default (options = {}, elId = 'some-el-id') => {
+  const $el = document.getElementById(elId)
+
+  if ($el) {
+    new Vue({
+      render: h => h(MyComponent, {
+        attrs: {
+          id: elId
+        },
+        props: {
+          ...options.props
+        }
+      })
+    }).$mount($el)
+  }
 }
